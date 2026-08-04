5bf224931215579de461068c766616f487290b95
Harumi Sagawa <h_sagawa@yumemi.co.jp>
2026-08-05 08:32:31 +0900

    Initial Commit

---
+++ SakataChartQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SakataChartQuiz
+//
+//  Created by harumi.sagawa on 2026/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SakataChartQuiz/SakataChartQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakataChartQuizApp.swift
+//  SakataChartQuiz
+//
+//  Created by harumi.sagawa on 2026/08/05.
+//
+
+import SwiftUI
+
+@main
+struct SakataChartQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
